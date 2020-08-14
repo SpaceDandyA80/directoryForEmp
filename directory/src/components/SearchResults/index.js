abe366ad362c0e2a014c200dec8b1a251b0454e5
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-08-13 20:47:10 -0700

    minor changes

--- directory/src/components/SearchResults/index.js
+++ directory/src/components/SearchResults/index.js
@@ -3,12 +3,12 @@ import SearchContext from '../../utils/SearchContext';
 
 
 function SearchResults() {
-    const Search = useContext(SearchContext);
+    const search = useContext(SearchContext);
     return(
         <ul className="list-group search-results">
         <li className="list-group-item">
-          <h2>{Search.title}</h2>
-          <a href={Search.url}>{Search.url}</a>
+          <h2>{search.title}</h2>
+          <a href={search.url}>{search.url}</a>
         </li>
       </ul>
     )
